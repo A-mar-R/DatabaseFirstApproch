26cf28e26173b1b0a8976b1ef4317231f9e23aea
A-ma-R <amarjadhav9022@gmail.com>
2026-05-29 16:20:06 +0530

    Data base first Approch

--- DatabaseFirstApproch/Controllers/HomeController.cs
+++ DatabaseFirstApproch/Controllers/HomeController.cs
@@ -1,33 +1,149 @@
-using DatabaseFirstApproch.Models;
+
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using DatabaseFirstApproch.Models;
 
-namespace DatabaseFirstApproch.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly StudentsDbContext _context;
+
+    public HomeController(StudentsDbContext context)
     {
-        private readonly StudentsDbContext context;
+        _context = context;
+    }
 
-        public HomeController(StudentsDbContext context)
+    // GET: STUDENTS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Students.ToListAsync());
+    }
+
+    // GET: STUDENTS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            this.context = context;
+            return NotFound();
         }
 
-        public IActionResult Index()
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (student == null)
         {
-            var data = context.Students.ToList();
-            return View(data);
+            return NotFound();
         }
 
-        public IActionResult Privacy()
+        return View(student);
+    }
+
+    // GET: STUDENTS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: STUDENTS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,StudentName,StudentGender,Age,Standard")] Student student)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(student);
+    }
+
+    // GET: STUDENTS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return NotFound();
         }
+        return View(student);
+    }
+
+    // POST: STUDENTS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("Id,StudentName,StudentGender,Age,Standard")] Student student)
+    {
+        if (id != student.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(student.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(student);
+    }
+
+    // GET: STUDENTS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+
+    // POST: STUDENTS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool StudentExists(int? id)
+    {
+        return _context.Students.Any(e => e.Id == id);
     }
 }
--- DatabaseFirstApproch/Program.cs
+++ DatabaseFirstApproch/Program.cs
@@ -6,31 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var provider =builder.Services.BuildServiceProvider();
-var config = provider.GetRequiredService<IConfiguration>();
-builder.Services.AddDbContext<StudentsDbContext>(item => item.UseSqlServer(config.GetConnectionString("dbcs")));
+
+
+// Register DbContext using the connection string from appsettings.json
+builder.Services.AddDbContext<StudentsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Student/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
